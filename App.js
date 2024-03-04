d7def20e90f6bca1de98fc2a0a434330ffdf93dc
Vaishali <aggarwal.vaishali785@gmail.com>
2024-03-04 18:44:53 +0530

    first file with  native code

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [goals, setGoals] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+
+
+    const handleSubmit = (enteredGoalText) => {
+        setGoals(prevGoals => [...prevGoals, { text: enteredGoalText, id: Math.random().toString() }]);
+        closeModal();
+    }
+
+    const deleteGoalHandler = (id) => {
+        setGoals(currentGoals => currentGoals.filter(goal => goal.id != id))
+    }
+    const closeModal = () => {
+        setShowModal(false);
+    }
+
+    return (
+        <View style={styles.appContainer}>
+            <Button title="Add New Goal" color="indigo" onPress={() => setShowModal(true)} />
+
+            <GoalInput visible={showModal} handleSubmit={handleSubmit} closeModal={closeModal} />
+            <View style={styles.goalsContainer}>
+                <FlatList
+                    data={goals}
+                    renderItem={(itemData) => {
+                        return <GoalItem text={itemData.item.text} onDelete={deleteGoalHandler} id={itemData.item.id} />
+                    }}
+                    keyExtractor={(item, index) => {
+                        return item.id
+                    }}
+                    alwaysBounceVertical={false}
+                />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        paddingTop: 50,
+        paddingHorizontal: 15,
+        flex: 1
+    },
+
+    goalsContainer: {
+        flex: 5,
+        marginTop: 8
+    },
+
 });
